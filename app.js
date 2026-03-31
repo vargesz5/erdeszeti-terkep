8344050881cf3387e903c432f86a62e5f3777339
vargesz_5 <vmate14538@gmail.com>
2026-03-31 08:54:42 +0200

    Zoomoláskor is célkereszt a középen marad

--- app.js
+++ app.js
@@ -135,12 +135,18 @@ function updateDisplayCoordsFromCenter() {
 }
 
 function onZoomEnd() {
-    if (map.getZoom() > 22) {
-        map.setZoom(22);
+    if (map.getZoom() > 21) {
+        map.setZoom(21);
     }
     if (map.getZoom() < 5) {
         map.setZoom(5);
     }
+    
+    if (!followMode && positionMarker) {
+        positionMarker.setLatLng(map.getCenter());
+        updateDisplayCoordsFromCenter();
+    }
+    
     loadBoundaries();
 }
 
